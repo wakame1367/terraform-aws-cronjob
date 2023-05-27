ec85c04b9e00ee48f53bb6a85a735a1526793ad7
wakamezake <hotwater1367@gmail.com>
2023-05-27 20:55:01 +0900

    init

---
+++ provider.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+
