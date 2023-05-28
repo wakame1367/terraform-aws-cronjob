a3c6e20f7b0f7acbf5c9880dbd30d939ec8ba00f
wakamezake <hotwater1367@gmail.com>
2023-05-28 16:38:03 +0900

    typo

--- envs/production/provider.tf
+++ envs/production/provider.tf
@@ -3,18 +3,19 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.4.6"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.52.0"
     }
-    cloud {
-      organization = "wakame"
+  }
+  cloud {
+    organization = "wakame"
 
-      workspaces {
-        name = "aws-cronjob"
-      }
+    workspaces {
+      name = "aws-cronjob"
     }
   }
-  required_version = "1.4.6"
 }
